0f0b4e1a56bc1adf80cb0e94edf842b5fbba36e9
Mr0Inka <thomas0weiss@googlemail.com>
2025-11-25 22:01:41 +0100

    added pages for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/offset-controller/',   // <-- ADD THIS LINE
   server: {
-    host: '0.0.0.0', // Allow connections from local network
+    host: '0.0.0.0',
     port: 3000,
   },
   build: {
